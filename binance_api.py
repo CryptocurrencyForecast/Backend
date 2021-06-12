1062ade80d0061b734fba20b6dc36943e5f50473
Louis <louis.dutoitld@gmail.com>
2021-06-13 00:26:26 +0200

    add binance api

---
+++ binance_api.py
@@ -0,0 +1,12 @@
+from binance.client import Client
+print("teeeetetetetteet")
+# API Key: UimWOZ9hTGnsH1K2iIad65HcDCM0wMcvhpV8i7WzhxKyT17s3CDk4YLEZIsRJfBM
+# Secret Key: XVtmEKyTUeP3hxw6xmWjWWWWf2DWPSgyQXxSFrrQMt3TjL8vYGryyjzLBiYfQYa3
+
+api_key = 'UimWOZ9hTGnsH1K2iIad65HcDCM0wMcvhpV8i7WzhxKyT17s3CDk4YLEZIsRJfBM'
+api_secret = 'XVtmEKyTUeP3hxw6xmWjWWWWf2DWPSgyQXxSFrrQMt3TjL8vYGryyjzLBiYfQYa3'
+
+client = Client(api_key, api_secret, testnet=True)
+
+info = client.get_all_tickers()
+print(info)
