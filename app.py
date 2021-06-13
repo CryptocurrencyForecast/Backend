51d7826276b6703e4fed6f4abe2f003e5121dded
Louis <louis.dutoitld@gmail.com>
2021-06-13 10:39:31 +0200

    get balance route

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, make_response
+from binance_api import get_account_balance
 from reddit import get_last_day_post_for_ticker
 from flask_cors import CORS
 
@@ -12,3 +13,9 @@ def get_posts_for_ticker(ticker):
     r.mimetype = 'application/json'
     return r
 
+@app.route('/balance/<token>', methods=['GET'])
+def get_balance(token):
+    data = get_account_balance(token)
+    r = make_response(data)
+    r.mimetype = 'application/json'
+    return r
--- binance_api.py
+++ binance_api.py
@@ -1,14 +1,18 @@
 from binance.client import Client
 import json
 
-# TESTNET CONFIG
-api_key = 'UimWOZ9hTGnsH1K2iIad65HcDCM0wMcvhpV8i7WzhxKyT17s3CDk4YLEZIsRJfBM'
-api_secret = 'XVtmEKyTUeP3hxw6xmWjWWWWf2DWPSgyQXxSFrrQMt3TjL8vYGryyjzLBiYfQYa3'
-client = Client(api_key, api_secret, testnet=True)
 
-# Loading balances
-balance = client.get_account()
-[balance.pop(k) for k in list(balance.keys()) if k != 'balances']
+def get_account_balance(token):
+    # TESTNET CONFIG
+    api_key = token[:64]
+    api_secret = token[64:]
+    client = Client(api_key, api_secret, testnet=True)
 
-b_json = json.dumps(balance)
-print(b_json)
+    # Loading balances
+    balance = client.get_account()
+    [balance.pop(k) for k in list(balance.keys()) if k != 'balances']
+
+    return json.dumps(balance)
+
+# api_key = 'UimWOZ9hTGnsH1K2iIad65HcDCM0wMcvhpV8i7WzhxKyT17s3CDk4YLEZIsRJfBM'
+# api_secret = 'XVtmEKyTUeP3hxw6xmWjWWWWf2DWPSgyQXxSFrrQMt3TjL8vYGryyjzLBiYfQYa3'
